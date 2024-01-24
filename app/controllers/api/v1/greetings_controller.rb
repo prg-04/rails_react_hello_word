8159a16d10756673a97dff06b8825c2bbc7192f4
prg-04 <karanjaevans95@gmail.com>
2024-01-24 05:04:59 +0300

    Fix rubocop error

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,7 +1,11 @@
-class Api::V1::GreetingsController < ApplicationController
-  # GET /greetings or /greetings.json
-  def index
-    @greetings = [Greeting.order('RANDOM()').first]
-    render json: @greetings
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      # GET /greetings or /greetings.json
+      def index
+        @greetings = [Greeting.order('RANDOM()').first]
+        render json: @greetings
+      end
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -12,11 +14,11 @@ Greeting.destroy_all
 
 # Array of greeting
 greetings = [
-  "Hello, welcome to our platform!",
-  "Greetings from our team!",
+  'Hello, welcome to our platform!',
+  'Greetings from our team!',
   "Hi there! We're glad you're here.",
-  "Welcome aboard!",
-  "Warm greetings to all our new users."
+  'Welcome aboard!',
+  'Warm greetings to all our new users.'
 ]
 
 # Create 5 greetings
